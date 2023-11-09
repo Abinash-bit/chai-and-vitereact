df084ddb6a7ecc7aad59c23adc899bb81e3784be
Abinash-bit <abinashmahapatra41@gmail.com>
2023-11-09 20:38:20 +0530

    Clean Code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ function App() {
   
 
   return (
+    <>
     <Chai/>
+    <h1>Chai aur react</h1>
+    <p>test para</p>
+    </>
 
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,15 @@ const anotherElement = (
     <a href="https://google.com" target='_blank'>Visit google</a>
 )
 
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blanck'},
+    'click me to visit google'
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    anotherElement
+    <App/>
   
 )
